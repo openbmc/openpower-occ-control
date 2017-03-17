2a46d78f54a475801db51789605c4af71c9e4eb5
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-03-17 05:01:00 -0500

    Implement org.open_power.OCC.PassThrough
    Implement the org.open_power.OCC.PassThrough d-bus interface.
    
    This d-bus interface is implemented by the
    open_power::occ::pass_through::PassThrough class. This commit has the
    initial implementation for the PassThrough ctor.
    
    Change-Id: I294529f08dcf65594e735ee5aeefdb37a2920b01
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ app.cpp
@@ -0,0 +1,19 @@
+#include <sdbusplus/bus.hpp>
+#include "config.h"
+#include "occ_pass_through.hpp"
+
+int main(int argc, char* argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+    sdbusplus::server::manager::manager objManager(bus,
+                                                   OCC_PASS_THROUGH_ROOT);
+    bus.request_name(OCC_PASS_THROUGH_BUSNAME);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
+    return 0;
+}
