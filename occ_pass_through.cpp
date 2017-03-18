f9e9177d3f51a681afa8793521e535ffda0630f7
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-03-18 01:09:28 -0500

    Implement open_power::occ::pass_through::run()
    The run() method claims the occ pass-through bus name and puts occ
    pass-through objects on the bus.
    
    Change-Id: I6bce9e609b7b977418bcfee15e97432cb7d9e6b3
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- occ_pass_through.cpp
+++ occ_pass_through.cpp
@@ -1,4 +1,7 @@
+#include <memory>
+#include <iostream>
 #include "occ_pass_through.hpp"
+#include "occ_finder.hpp"
 
 namespace open_power
 {
@@ -6,6 +9,30 @@ namespace occ
 {
 namespace pass_through
 {
+
+void run()
+{
+    auto bus = sdbusplus::bus::new_default();
+    sdbusplus::server::manager::manager objManager(bus,
+                                                   OCC_PASS_THROUGH_ROOT);
+
+    std::vector<std::unique_ptr<PassThrough>> objects;
+    auto occs = open_power::occ::finder::get();
+
+    for (const auto& occ : occs)
+    {
+        auto occPassThrough = object(occ);
+        objects.emplace_back(
+            std::make_unique<PassThrough>(bus, occPassThrough.c_str()));
+    }
+    bus.request_name(OCC_PASS_THROUGH_BUSNAME);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+}
 
 PassThrough::PassThrough(
     sdbusplus::bus::bus& bus,
