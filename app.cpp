fd358d18b8b7f73204cc482f3b29f49a50fda214
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-05-08 07:11:22 -0500

    Create OCC pass-through objects
    Create OCC pass-through d-bus objects when corresponding CPU objects are
    created in the inventory.
    
    Resolves openbmc/openbmc#1450.
    
    Change-Id: I8da879f51ebef8dcc3d25358def81c5e0dce0617
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- app.cpp
+++ app.cpp
@@ -6,7 +6,19 @@ int main(int argc, char* argv[])
 {
     try
     {
-        open_power::occ::pass_through::run();
+        auto bus = sdbusplus::bus::new_default();
+        bus.request_name(OCC_PASS_THROUGH_BUSNAME);
+
+        sdbusplus::server::manager::manager objManager(bus,
+                                                       OCC_PASS_THROUGH_ROOT);
+
+        open_power::occ::pass_through::manager::Manager mgr(bus);
+
+        while (true)
+        {
+            bus.process_discard();
+            bus.wait();
+        }
     }
     catch (const std::exception& e)
     {
