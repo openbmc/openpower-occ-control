c70a94e4235e649658daef396bbf8e45d6ac53b8
Andrew Geissler <andrewg@us.ibm.com>
2017-06-20 15:46:52 -0500

    Register callbacks for pcap property changes
    Change-Id: I39b38a931ffdf260d9ee45f02cdd31e9e884b04d
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

---
+++ powercap.cpp
@@ -0,0 +1,29 @@
+#include <powercap.hpp>
+#include <phosphor-logging/log.hpp>
+
+namespace open_power
+{
+namespace occ
+{
+namespace powercap
+{
+
+using namespace phosphor::logging;
+
+
+void PowerCap::pcapChanged(sdbusplus::message::message& msg)
+{
+    log<level::DEBUG>("Power Cap Change Detected");
+    if(!occStatus.occActive())
+    {
+        // Nothing to  do
+        return;
+    }
+    // TODO - Process this change
+}
+
+} // namespace open_power
+
+} // namespace occ
+
+}// namespace powercap
