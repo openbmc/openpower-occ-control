3f80a6942f18d3d384e02c3cf16516f8bedb3957
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-06-28 19:17:28 +0530

    Add Support to do device bind and unbind based on OCC status
    Change-Id: Iaa33c3065b0358b62e3ec7e39d4b57c6b9d181e1
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- occ_status.cpp
+++ occ_status.cpp
@@ -7,6 +7,19 @@ namespace occ
 // Handles updates to occActive property
 bool Status::occActive(bool value)
 {
+    if (value != this->occActive())
+    {
+        if (value)
+        {
+            // Bind the device
+            device.bind();
+        }
+        else
+        {
+            // Do the unbind
+            device.unBind();
+        }
+    }
     return Base::Status::occActive(value);
 }
 
