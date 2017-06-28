307d80b3d78efc669f38a63938371adcc6934391
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-06-28 15:56:09 +0530

    Implement OCC status interface
    Change-Id: I43822cb28bc2d23398fb09352c9876e169c666f7
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ occ_status.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <org/open_power/OCC/Status/server.hpp>
+namespace open_power
+{
+namespace occ
+{
+
+namespace Base = sdbusplus::org::open_power::OCC::server;
+using Interface = sdbusplus::server::object::object<Base::Status>;
+
+/** @class Status
+ *  @brief Implementation of OCC Active Status
+ */
+class Status : public Interface
+{
+    public:
+        Status() = delete;
+        ~Status() = default;
+        Status(const Status&) = delete;
+        Status& operator=(const Status&) = delete;
+        Status(Status&&) = default;
+        Status& operator=(Status&&) = default;
+
+        /** @brief Constructs the Status object
+         *
+         *  @param[in] bus  - DBus bus to attach to
+         *  @param[in] path - DBus object path
+         */
+        Status(sdbusplus::bus::bus& bus, const char* path)
+            : Interface(bus, path)
+        {
+            // Nothing to do here
+        }
+
+        /** @brief SET OccActive to True or False
+         *
+         *  @param[in] value - Intended value
+         *
+         *  @return          - Updated value of the property
+         */
+        bool occActive(bool value) override;
+};
+
+} // namespace occ
+} // namespace open_power
