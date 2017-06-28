450e55bd81eba8fb88b3ec150064c448455c641f
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-06-28 19:17:28 +0530

    Add Support to do device bind and unbind based on OCC status
    Change-Id: Iaa33c3065b0358b62e3ec7e39d4b57c6b9d181e1
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ occ_device.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <fstream>
+#include "config.h"
+namespace open_power
+{
+namespace occ
+{
+
+/** @class Device
+ *  @brief Binds and unbinds the OCC driver upon request
+ */
+class Device
+{
+    public:
+        Device() = delete;
+        ~Device() = default;
+        Device(const Device&) = delete;
+        Device& operator=(const Device&) = delete;
+        Device(Device&&) = default;
+        Device& operator=(Device&&) = default;
+
+        /** @brief Constructs the Device object
+         *
+         *  @param[in] name - OCC instance name
+         */
+        Device(const std::string& name) :
+            config(name + '-' + "dev0"),
+            errorFile(config + '/' + "occ_error"),
+            bindPath(std::string(OCC_HWMON_PATH) + "bind"),
+            unBindPath(std::string(OCC_HWMON_PATH) + "unbind")
+        {
+            // Nothing to do here
+        }
+
+        /** @brief Binds device to the OCC driver */
+        inline void bind()
+        {
+            return write(bindPath, config);
+        }
+
+        /** @brief Un-binds device from the OCC driver */
+        inline void unBind()
+        {
+            return write(unBindPath, config);
+        }
+
+    private:
+        /** @brief Config value to be used to do bind and unbind */
+        std::string config;
+
+        /** @brief This file contains 0 for success and non-zero for failure */
+        std::string errorFile;
+
+        /**  @brief To bind the device to the OCC driver, do:
+         *    Write occ<#>-dev0 to: /sys/bus/platform/drivers/occ-hwmon/bind
+         */
+        std::string bindPath;
+
+        /**  @brief To un-bind the device from the OCC driver, do:
+         *    Write occ<#>-dev0 to: /sys/bus/platform/drivers/occ-hwmon/unbind
+         */
+        std::string unBindPath;
+
+        /** @brief Generic file writer to achieve bind and unbind
+         *
+         *  @param[in] filename - Name of file to be written
+         *  @param[in] data     - Data to be written to
+         *  @return             - None
+         */
+        template <typename T>
+        void write(const std::string& fileName, T&& data)
+        {
+            if(std::ifstream(fileName))
+            {
+                std::ofstream file(fileName, std::ios::out);
+                file << data;
+                file.close();
+            }
+            return;
+        }
+};
+
+} // namespace occ
+} // namespace open_power
