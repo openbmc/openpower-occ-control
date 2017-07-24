4b34f2f60754f7b1fc7d51b448eed9f0a5f6226f
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-07-24 23:13:14 +0530

    Reset OCC on error condition
    After detecting the error in the OCC, invokes OccReset command
    in Host Control dbus object with the sensor ID of the failing OCC
    
    Fixes openbmc/openbmc#1363
    
    Change-Id: I608dbbb943d3b39d0709d6f350ab799e771a13e9
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ utils.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <string>
+namespace open_power
+{
+namespace occ
+{
+/**
+ * @brief Gets the D-Bus Service name for the input D-Bus path
+ *
+ * @param[in] bus  -  Bus handler
+ * @param[in] intf -  Interface
+ * @param[in] path -  Object Path
+ *
+ * @return            Service name
+ * @error             InternalFailure exception thrown
+ */
+std::string getService(sdbusplus::bus::bus& bus,
+                       const std::string& intf,
+                       const std::string& path);
+} // namespace occ
+} // namespace open_power
