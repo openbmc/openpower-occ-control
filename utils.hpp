18dc1281fe5af83c1dbb9c8895d7af824d2c1856
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-08-29 14:09:35 +0530

    Change signature of getService
    getService() has [bus, interface, path] as parameter and
    the caller invoked it with [bus, path, interface] and that
    resulted in a failure. Although the fix can be put into the
    caller to pass the right arguments, better fix would be to
    change the signature of getService() to receive path and then
    the interface as that is what has been followed all over and
    the deviation resulted in this bug.
    
    Change-Id: I3efe7f4f0a2d0bceb7e6e801cd68de6b2624fbe0
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- utils.hpp
+++ utils.hpp
@@ -10,14 +10,14 @@ namespace occ
  * @brief Gets the D-Bus Service name for the input D-Bus path
  *
  * @param[in] bus  -  Bus handler
- * @param[in] intf -  Interface
  * @param[in] path -  Object Path
+ * @param[in] intf -  Interface
  *
  * @return            Service name
  * @error             InternalFailure exception thrown
  */
 std::string getService(sdbusplus::bus::bus& bus,
-                       const std::string& intf,
-                       const std::string& path);
+                       const std::string& path,
+                       const std::string& intf);
 } // namespace occ
 } // namespace open_power
