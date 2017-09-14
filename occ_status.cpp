2dab378a941a4e0d4164544ce83045a264b82411
Eddie James <eajames@us.ibm.com>
2017-09-14 13:17:17 -0500

    Add watches for throttling reported by the OCC
    Add Error objects to watch the sysfs entries provided by the OCC hwmon
    driver that report various types of throttling. Also needed to add a
    boolean input to the Error callback interface.
    
    Resolves openbmc/openbmc#1821
    
    Change-Id: I4425770a92ace0f73024b3dc4c577ce46957a62a
    Signed-off-by: Eddie James <eajames@us.ibm.com>

--- occ_status.cpp
+++ occ_status.cpp
@@ -42,13 +42,17 @@ bool Status::occActive(bool value)
 }
 
 // Callback handler when a device error is reported.
-void Status::deviceErrorHandler()
+void Status::deviceErrorHandler(bool error)
 {
-    // This would deem OCC inactive
-    this->occActive(false);
+    // Make sure we have an error
+    if (error)
+    {
+        // This would deem OCC inactive
+        this->occActive(false);
 
-    // Reset the OCC
-    this->resetOCC();
+        // Reset the OCC
+        this->resetOCC();
+    }
 }
 
 // Sends message to host control command handler to reset OCC
