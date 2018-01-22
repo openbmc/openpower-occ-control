8bdcb2e0ed46aa21ae8938269a1484db10bb40f7
Eddie James <eajames@us.ibm.com>
2018-01-22 14:40:49 -0600

    Retry OCC bind operations
    OCC communication can get intermittent failures. Retry all binds 3 times
    before declaring a failure and exiting out of the openpower-occ-control
    application.
    
    Resolves openbmc/openbmc#2779
    
    Change-Id: Iec0e6f975d1014ffcf9ac7631c6350b81cc496be
    Signed-off-by: Eddie James <eajames@us.ibm.com>

--- occ_device.cpp
+++ occ_device.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <phosphor-logging/log.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <org/open_power/OCC/Device/error.hpp>
+#include "elog-errors.hpp"
 #include "occ_device.hpp"
 #include "occ_status.hpp"
 
@@ -9,6 +18,48 @@ namespace occ
 
 fs::path Device::bindPath = fs::path(OCC_HWMON_PATH) / "bind";
 fs::path Device::unBindPath = fs::path(OCC_HWMON_PATH) / "unbind";
+
+void Device::write(const fs::path& fileName, const std::string& data)
+{
+    using namespace phosphor::logging;
+    using namespace sdbusplus::org::open_power::OCC::Device::Error;
+
+    auto retries = 3;
+    auto fd = open(fileName.c_str(), O_WRONLY);
+
+    if (fd < 0)
+    {
+        elog<OpenFailure>(
+            phosphor::logging::org::open_power::OCC::Device::
+                OpenFailure::CALLOUT_ERRNO(errno),
+            phosphor::logging::org::open_power::OCC::Device::
+                OpenFailure::CALLOUT_DEVICE_PATH(fileName.c_str()));
+        return;
+    }
+
+    // OCC / FSI have intermittent issues so retry all binds
+    while (true)
+    {
+        auto rc = ::write(fd, data.data(), data.size());
+        if (rc < 0)
+        {
+            retries--;
+            if (retries == 0)
+            {
+                elog<WriteFailure>(
+                    phosphor::logging::org::open_power::OCC::Device::
+                        ReadFailure::CALLOUT_ERRNO(errno),
+                    phosphor::logging::org::open_power::OCC::Device::
+                        ReadFailure::CALLOUT_DEVICE_PATH(fileName.c_str()));
+                break;
+            }
+            continue;
+        }
+        break;
+    }
+
+    close(fd);
+}
 
 bool Device::master() const
 {
