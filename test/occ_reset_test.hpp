be7a9332a38cb607601e244497c19a1e7cf0c046
vkaverap <vkaverap@in.ibm.com>
2020-04-26 22:57:39 -0500

    PLDM: OCC Reset path : updates to occ control app
    OCC reset was previously done only with an IPMI msg. With this
    commit we do a PLDM based reset of OCC. Dbus API is invoked to
    retrieve the state effecter PDR for the OCC that has to be reset.
    State effecter PDR is encoded and sent to initiate the reset.
    
    Change-Id: I5ff18e817195f55567f9391ee05cf7855e6c2e6f
    Signed-off-by: vkaverap <vkaverap@in.ibm.com>

---
+++ test/occ_reset_test.hpp
@@ -0,0 +1,14 @@
+#include <libpldm/base.h>
+#include <libpldm/platform.h>
+#include <libpldm/pldm.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+class MockPldmSendRecv
+{
+  public:
+    MOCK_METHOD(uint8_t, pldm_send_recv,
+                (uint8_t, int, const uint8_t*, size_t, uint8_t**, size_t*),
+                (const override));
+};
