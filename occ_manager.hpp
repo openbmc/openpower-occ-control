8ba8c9c0de1453b1895e5400f5b23c262eb5fc53
Chicago Duan <duanzhijia01@inspur.com>
2021-06-18 19:37:16 +0800

    Read proc temps and dimm temps
    This commit uses openpower-occ-control to monitor the temperature of
    processor and memory, and create temperature sensors on D-BUS. In the
    loop, read the CPU and memory temperatures every 1 seconds.
    
    Tested:
    I use virtual data to get dimm and cpu core temperature:
    busctl tree org.open_power.OCC.Control
    |-/org
    | `-/org/open_power
    |   `-/org/open_power/control
    |     |-/org/open_power/control/occ0
    |     `-/org/open_power/control/occ1
    `-/xyz
      `-/xyz/openbmc_project
        `-/xyz/openbmc_project/sensors
          `-/xyz/openbmc_project/sensors/temperature
            |-/xyz/openbmc_project/sensors/temperature/dimm10_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/dimm11_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/dimm2_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/dimm3_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/dimm4_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/dimm5_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/dimm9_dram_temp
            |-/xyz/openbmc_project/sensors/temperature/proc0_core2_temp
            |-/xyz/openbmc_project/sensors/temperature/proc0_core3_temp
            |-/xyz/openbmc_project/sensors/temperature/proc1_core2_temp
            `-/xyz/openbmc_project/sensors/temperature/proc1_core3_temp
    
    busctl introspect org.open_power.OCC.Control
    /xyz/openbmc_project/sensors/temperature/proc0_core3_temp
    NAME                                TYPE      SIGNATURE RESULT/VALUE
    org.freedesktop.DBus.Introspectable interface -         -
    .Introspect                         method    -         s
    org.freedesktop.DBus.Peer           interface -         -
    .GetMachineId                       method    -         s
    .Ping                               method    -         -
    org.freedesktop.DBus.Properties     interface -         -
    .Get                                method    ss        v
    .GetAll                             method    s         a{sv}
    .Set                                method    ssv       -
    .PropertiesChanged                  signal    sa{sv}as  -
    xyz.openbmc_project.Sensor.Value    interface -         -
    .MaxValue                           property  d         0
    .MinValue                           property  d         0
    .Unit                               property  s         "xyz.openbmc...
    .Value                              property  d         49
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: Iff30ab51745dab500fa19aa4c35b07e0052ac665

--- occ_manager.hpp
+++ occ_manager.hpp
@@ -48,7 +48,11 @@ struct Manager
         _pollTimer(
             std::make_unique<
                 sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>>(
-                sdpEvent, std::bind(&Manager::pollerTimerExpired, this)))
+                sdpEvent, std::bind(&Manager::pollerTimerExpired, this))),
+        _tempPollTimer(
+            std::make_unique<
+                sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>>(
+                sdpEvent, std::bind(&Manager::tempPollerTimerExpired, this)))
 #ifdef PLDM
         ,
         pldmHandle(std::make_unique<pldm::Interface>(
@@ -139,6 +143,15 @@ struct Manager
         sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>>
         _pollTimer;
 
+    /**
+     * @brief The temperature poll timer to be used once the OCC goes active.
+     * When it expires, the dimm and processor temperatures will be got and then
+     * timer restarted.
+     */
+    std::unique_ptr<
+        sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>>
+        _tempPollTimer;
+
 #ifdef I2C_OCC
     /** @brief Init Status objects for I2C OCC devices
      *
@@ -170,6 +183,34 @@ struct Manager
      * OCC. The poll timer will then be restarted.
      * */
     void pollerTimerExpired();
+
+    /**
+     * @brief Called when temperature poll timer expires and gets dimm and
+     * processor temperatures from the OCC. The temperature poll timer will then
+     * be restarted.
+     * */
+    void tempPollerTimerExpired();
+
+    /**
+     * @brief Gets the temperatures of the processor and dimm.
+     * */
+    void getProcDimmTemp();
+
+    /**
+     * @brief Trigger OCC driver to read the temperatures of processor and dimm.
+     * @param[in] path - path of the OCC sensors.
+     * @param[in] id - Id of the OCC.
+     * */
+    void readProcDimmTemp(const fs::path& path, uint8_t id);
+
+    /**
+     * @brief Set all the temperatures of this OCC to NaN.
+     * @param[in] id - Id of the OCC.
+     * */
+    void setTempToNaN(uint8_t id);
+
+    /** @brief Store the existing OCC sensors on D-BUS */
+    std::map<std::string, uint8_t> existingSensors;
 };
 
 } // namespace occ
